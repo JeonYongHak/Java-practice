c5d721ca18e8b9bbf722065c7865bfd0c0adb69b
JeonYongHak <charismayoung1993@gmail.com>
2024-11-19 21:15:10 +0900

    변수

---
+++ Comment.java
@@ -0,0 +1,10 @@
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java를 출력합니다.
+        System.out.println("hello java2"); // hello java2를 출력합니다.
+/*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+*/
+    }
+}
