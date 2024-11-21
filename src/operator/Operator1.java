c164462486ffe4c6be25a9b383422fe187853d96
JeonYongHak <charismayoung1993@gmail.com>
2024-11-21 15:48:34 +0900

    연산자 증감 까지

---
+++ src/operator/Operator1.java
@@ -0,0 +1,31 @@
+package src.operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        // 변수 초기화
+        int a = 5;
+        int b= 2;
+
+        // 덧셈
+
+        int sum = a + b;
+        System.out.println("a + b = " + sum); // 출력 a + b = 3
+
+        // 뺄셈
+        int diff = a - b;
+        System.out.println("a - b = " + diff); // 출력 a -b =3;
+        // 곱셈
+        int multi = a *b;
+        System.out.println(multi);
+
+        //나눗셈
+        double div= a /b ;
+        System.out.println(div);
+        //나머지
+        int mod = 5 % 2;
+        System.out.println(mod); // 몫을 제외하고 나머지가 출력이 됨
+
+
+
+    }
+}
---
+++ src/operator/OperatorAdd1.java
@@ -0,0 +1,7 @@
+package src.operator;
+
+public class OperatorAdd1 {
+    public static void main(String[] args) {
+        int a = 0;
+    }
+}
